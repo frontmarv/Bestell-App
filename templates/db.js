4543f25f306d1c701e2a481b8cfac1fc7bbd9044
marv_len <marvin.lenhart@gmx.net>
2025-09-11 10:39:35 +0200

    db mit items gefüllt

---
+++ templates/db.js
@@ -0,0 +1,77 @@
+let orderItems = {
+    "pizza": {
+        "titleImage": "images/pizza.jpg",
+        "items": [
+            {
+                "name": "Margherita",
+                "description": "Klassische Pizza mit Tomatensauce, Mozzarella und frischem Basilikum.",
+                "price": 7.50
+            },
+            {
+                "name": "Salami",
+                "description": "Herzhafte Salami auf würziger Tomatensauce und feinem Mozzarella.",
+                "price": 8.50
+            },
+            {
+                "name": "Vegetariana",
+                "description": "Mit frischem Gemüse der Saison, Tomatensauce und Mozzarella.",
+                "price": 9.00
+            },
+            {
+                "name": "Prosciutto",
+                "description": "Belegt mit zartem Kochschinken, Tomatensauce und Mozzarella.",
+                "price": 9.50
+            }
+        ]
+    },
+    "burger": {
+        "titleImage": "images/pizza.jpg",
+        "items": [
+            {
+                "name": "Classic Burger",
+                "description": "Saftiges Rindfleisch-Patty mit Salat, Tomate, Gurke und Burger-Sauce.",
+                "price": 8.90
+            },
+            {
+                "name": "Cheeseburger",
+                "description": "Rindfleisch-Patty mit geschmolzenem Cheddar, Salat und Tomate.",
+                "price": 9.50
+            },
+            {
+                "name": "BBQ Bacon Burger",
+                "description": "Rindfleisch-Patty mit knusprigem Bacon, BBQ-Sauce und Röstzwiebeln.",
+                "price": 10.50
+            },
+            {
+                "name": "Veggie Burger",
+                "description": "Vegetarisches Patty mit Salat, Tomate, Gurke und hausgemachter Sauce.",
+                "price": 8.50
+            }
+        ]
+    },
+    "getraenke": {
+        "titleImage": "images/pizza.jpg",
+        "items": [
+            {
+                "name": "Cola",
+                "description": "Erfrischendes Cola-Getränk, gekühlt serviert.",
+                "price": 2.50
+            },
+            {
+                "name": "Wasser",
+                "description": "Mineralwasser – still oder sprudelnd.",
+                "price": 2.00
+            },
+            {
+                "name": "Eistee Pfirsich",
+                "description": "Fruchtiger Eistee mit Pfirsichgeschmack.",
+                "price": 2.80
+            },
+            {
+                "name": "Bier",
+                "description": "Frisch gezapftes, kühles Bier.",
+                "price": 3.50
+            }
+        ]
+    }
+}
