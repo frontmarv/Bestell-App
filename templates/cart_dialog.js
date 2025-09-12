39cc7e62e5f954c9a9d6723564bc063e0137a28c
marv_len <marvin.lenhart@gmx.net>
2025-09-12 20:10:22 +0200

    order button styled mobile cart issues done

--- templates/cart_dialog.js
+++ templates/cart_dialog.js
@@ -4,7 +4,7 @@ let mobileCart = document.getElementById("mainCart")
 dialog.addEventListener("click", (event) => {
     if (event.target === dialog) {
         closeDialog();
-    }
+    } else {renderDialog()}
 });
 
 function openDialog() {
@@ -15,10 +15,6 @@ function openDialog() {
 function renderDialog(){
     contentFormCart.innerHTML = mobileCart.innerHTML
 }
-
-
-
-
 
 function closeDialog() {
     dialog.close();
